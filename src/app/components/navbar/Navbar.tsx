7f243928f59d0bed737539f2a23fa48583928648
Abhinav Rastogi <rastogiabhinav@Abhinavs-MacBook-Pro.local>
2025-06-26 13:57:10 +0530

    Mobile Navbar added and pushed

--- src/app/components/navbar/Navbar.tsx
+++ src/app/components/navbar/Navbar.tsx
@@ -1,14 +1,29 @@
+import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import { GiHamburgerMenu } from "react-icons/gi";
 
-const Navbar = () => {
+interface NavProp {
+  setIsNavToggle: React.Dispatch<React.SetStateAction<boolean>>;
+}
+const Navbar = ({ setIsNavToggle }: NavProp) => {
   return (
     <div
-      className="flex items-center justify-between bg-teal h-[1rem] md:h-[3rem] rounded-xl text-white py-6 md:py-10 px-6 text-center"
+      className="flex items-center justify-between bg-teal h-[1rem] md:h-[3rem] rounded-xl text-white py-6 md:py-10 px-4 md:px-6 text-center"
       aria-label="Hero Section"
     >
       <div className="">
-        <h1 className="font-bold text-xl text-yellow">KEON PHARM</h1>
+        <div className="font-bold text-xl text-yellow">
+          <Link href="/">
+            <Image
+              src={"/images/img/brand.png"}
+              width={100}
+              height={100}
+              alt="brand"
+              className=" w-[50%] md:w-[80%]"
+            />
+          </Link>
+        </div>
       </div>
       <div className="menu hidden md:block">
         <ul className="font-semibold inline-flex gap-x-12">
@@ -16,19 +31,31 @@ const Navbar = () => {
             <Link href="/">Home</Link>
           </li>
           <li>
-            <Link href="/">About</Link>
+            <Link href="#we_are">About</Link>
           </li>
           <li>
-            <Link href="/">Our Services</Link>
+            <Link href="#vision">Our Vision</Link>
           </li>
           <li>
-            <Link href="/">Doctor</Link>
+            <Link href="#experts">Our Experts</Link>
           </li>
           <li>
-            <Link href="/">FAQ</Link>
+            <Link href="#focus">Expertise</Link>
+          </li>
+          <li>
+            <Link href="#choose">Advantage</Link>
+          </li>
+          <li>
+            <Link href="#contact">Contact us</Link>
           </li>
         </ul>
       </div>
+      <button
+        className="block md:hidden text-3xl "
+        onClick={() => setIsNavToggle(true)}
+      >
+        <GiHamburgerMenu />
+      </button>
       <div className="hidden md:block">
         <a
           href="#contact"
